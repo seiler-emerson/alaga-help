0e6efc8c3acb993edddf0cbfa28a4e7756273307
Emerson Seiler <seiler.emerson@gmail.com>
2024-12-27 12:59:28 -0300

    refactor(auth): restructure login flow and components

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,6 +1,4 @@
-
-
-import { GalleryVerticalEnd } from 'lucide-react';
+import { Waves } from 'lucide-react';
 import { AuthForm } from './_components/auth-form';
 import Image from 'next/image';
 
@@ -8,14 +6,16 @@ const Page = () => {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-              <GalleryVerticalEnd className="size-4" />
-            </div>
-            {process.env.NEXT_PUBLIC_SITE_NAME}
-          </a>
-        </div>
+        <a href="#" className='w-96 flex flex-row justify-center gap-2 md:justify-start'>
+          <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+            <Waves className="size-4" />
+          </div>
+          <div className="grid flex-1 text-left text-sm leading-tight">
+            <span className="truncate font-semibold">Alaga Help</span>
+            <span className="truncate text-xs">Monitoramento</span>
+          </div>
+        </a>
+
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
             <AuthForm />
--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -13,11 +13,6 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
   },
   adapter: PrismaAdapter(prisma),
   providers: [
-    // Nodemailer = opcao oficial na documentacao, mas apos enviar esta direcionando para outro form, ao inves de usar o form ja definido
-    // Nodemailer({
-    //   server: process.env.EMAIL_SERVER,
-    //   from: process.env.EMAIL_FROM,
-    // }),
     EmailProvider({
       server: process.env.EMAIL_SERVER,
       from: process.env.EMAIL_FROM,
