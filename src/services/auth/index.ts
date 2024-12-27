f62c63abba1504706940b2ea75d131e5bc28be0d
Emerson Seiler <seiler.emerson@gmail.com>
2024-12-27 13:44:00 -0300

    feat(auth): customize login email content and structure

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from '../database'
 import EmailProvider from "next-auth/providers/email"
+import { createTransport } from "nodemailer"
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   pages: {
@@ -16,6 +17,47 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     EmailProvider({
       server: process.env.EMAIL_SERVER,
       from: process.env.EMAIL_FROM,
+      sendVerificationRequest: async ({
+        identifier: email,
+        url,
+        provider: { server, from },
+      }) => {
+        const transport = createTransport(server)
+        const { host } = new URL(url)
+        
+        await transport.sendMail({
+          to: email,
+          from: from,
+          subject: `Faça Login no Alaga Help`,
+          text: `Clique no link para fazer login: ${url}`,
+          html: `
+            <div style="background: #f9f9f9; padding: 20px;">
+              <table style="max-width: 600px; margin: auto; background: white; border-radius: 10px; padding: 20px;">
+                <tr>
+                  <td style="text-align: center; padding: 20px;">
+                    <h1>Bem-vindo ao Alaga Help</h1>
+                    <p>Clique no botão abaixo para fazer login:</p>
+                    <a 
+                      href="${url}" 
+                      style="background: #346df1; color: white; padding: 12px 24px; text-decoration: none; border-radius: 5px; display: inline-block; margin: 20px 0px;"
+                    >
+                      Fazer Login
+                    </a>
+                    <p style="color: #666; font-size: 14px;">
+                      Se você não solicitou este email, pode ignorá-lo com segurança.
+                    </p>
+                     <a 
+                      href="https://github.com/seiler-emerson" 
+                    >
+                      Acesse o github do projeto aqui!
+                    </a>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          `
+        })
+      }
     }),
   ],
 })
