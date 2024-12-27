f5165b836cf975fc12af0a1dc5b339825fbe816b
Emerson Seiler <seiler.emerson@gmail.com>
2024-12-27 10:52:36 -0300

    chore(river-levels): add data source attribution for river level information

--- src/app/app/rivers-chart-blumenau/_components/chart.tsx
+++ src/app/app/rivers-chart-blumenau/_components/chart.tsx
@@ -216,18 +216,15 @@ export const RiverChart = ({ chartData, name }: Props) => {
                     </AreaChart>
                 </ChartContainer>
             </CardContent>
-            {/* <CardFooter>
+            <CardFooter>
                 <div className="flex w-full items-start gap-2 text-sm">
                     <div className="grid gap-2">
                         <div className="flex items-center gap-2 font-medium leading-none">
-                            Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-                        </div>
-                        <div className="flex items-center gap-2 leading-none text-muted-foreground">
-                            January - June 2024
+                            Fonte dos dados: <a href="https://alertablu.blumenau.sc.gov.br/d/nivel-do-rio" target='_blank'>AlertaBlu - Defesa Cívil - Blumenau-SC</a>
                         </div>
                     </div>
                 </div>
-            </CardFooter> */}
+            </CardFooter>
         </Card>
     );
 };
--- src/app/app/rivers-chart-itajai/_components/chart.tsx
+++ src/app/app/rivers-chart-itajai/_components/chart.tsx
@@ -182,18 +182,15 @@ export const RiverChart = ({ chartData, name }: Props) => {
                     </AreaChart>
                 </ChartContainer>
             </CardContent>
-            {/* <CardFooter>
+            <CardFooter>
                 <div className="flex w-full items-start gap-2 text-sm">
                     <div className="grid gap-2">
                         <div className="flex items-center gap-2 font-medium leading-none">
-                            Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-                        </div>
-                        <div className="flex items-center gap-2 leading-none text-muted-foreground">
-                            January - June 2024
+                            Fonte dos dados: <a href="https://defesacivil.itajai.sc.gov.br/monitoramento/nivel-rios" target='_blank'>Defesa Cívil - Itajaí-SC</a>
                         </div>
                     </div>
                 </div>
-            </CardFooter> */}
+            </CardFooter>
         </Card>
     );
 };
